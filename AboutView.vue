44356ef7f91c5c22b90c2aa4c3d1adefba806f11
Jesse <daniilgolosov232@gmail.com>
2026-06-11 10:48:15 +0300

    страница «О приложении»

---
+++ AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-card class="pa-6" max-width="600" elevation="2">
+      <v-card-title class="text-h4">О приложении</v-card-title>
+      <v-card-text>
+        <p>Это простое приложение для заметок, созданное с помощью Vue и Vuetify.</p>
+        <v-divider class="my-3"></v-divider>
+        <p class="text-caption">Версия 1.0.0</p>
+        <p class="text-caption">Дизайн в стиле Material Design от Google.</p>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" to="/">К заметкам</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
